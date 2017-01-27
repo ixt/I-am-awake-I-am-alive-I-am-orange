22cad8e07507a06f4719f9d58feb4b86491cc36d
NfN Orange <orange@ff4500.red>
2017-01-27 00:20:47 +0000

    Changed to cURL
    Signed-off-by: NfN Orange <orange@ff4500.red>

--- Commit.sh
+++ Commit.sh
@@ -5,12 +5,12 @@ git add README.md
 git commit -sm "$(date -u +%Y-%m-%d\ %H:%M:%S)"
 git push origin master
 git push ff4500 master
-gpg -d twitter.rb.gpg > twitter.rb
+gpg -d tweet.sh.gpg > tweet.sh
 while read entry; do
-   ./twitter.rb "$(echo $entry | sed -s "s/# //g")"
+   ./tweet.sh "$(echo $entry | sed -s "s/# //g")"
 done < README.md
 COMMITID=$(git log -1 | grep -i "commit" | sed -e "s/commit //g")
 COMMITURL=$(echo "https://github.com/ixt/I-am-awake-I-am-alive-I-am-orange/commit/$COMMITID")
-./twitter.rb "Verification: $COMMITURL"
-echo "" > twitter.rb
+./tweet.sh "Verification: $COMMITURL"
+echo "" > tweet.sh
 . torsocks off 
