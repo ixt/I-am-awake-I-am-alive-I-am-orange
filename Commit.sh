13626d8d362d421eead0cfd43ac0dffdbec0c936
NfN Orange <orange@ff4500.red>
2017-02-11 11:55:29 +0000

    changed which repos to commit to

--- Commit.sh
+++ Commit.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # 2017-01-16 23:26:02
+REPOS=( origin ff4500 github )
 . torsocks on
-git add README.md
-git commit -sm "$(date -u +%Y-%m-%d\ %H:%M:%S)"
-git push origin master
-git push ff4500 master
-git push github master
+git add README.md 2>/dev/null
+git commit -sm "$(date -u +%Y-%m-%d\ %H:%M:%S)" 2>/dev/null
+for i in "${REPOS[@]}"; do
+    pkill gpg-agent
+    gpg-agent --enable-ssh-support --daemon git push $i master
+done
 gpg -d tweet.sh.gpg > tweet.sh
 while read entry; do
    ./tweet.sh "$(echo $entry | sed -s "s/# //g")"
 done < README.md
-COMMITID=$(git log -1 | grep -i "commit" | sed -e "s/commit //g")
+COMMITID=$(git log -1 2>/dev/null | grep -i "commit" | sed -e "s/commit //g")
 COMMITURL=$(echo "https://github.com/ixt/I-am-awake-I-am-alive-I-am-orange/commit/$COMMITID")
 ./tweet.sh "Verification: $COMMITURL"
 echo "" > tweet.sh
