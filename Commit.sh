33e290a0a8e21a7d9bd19d53a2d8ed2f32256007
NfN Orange <orange@ff4500.red>
2017-02-12 01:07:50 +0000

    hopefully more fixes

--- Commit.sh
+++ Commit.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # 2017-02-12 00:52:24
+. torsocks on
 git add README.md 2>/dev/null
 git commit -sm "$(date -u +%Y-%m-%d\ %H:%M:%S)" 2>/dev/null
 for r in $(git remote); do git push $r master; done
+gpg --card-status 2>/dev/null
 gpg -d tweet.sh.gpg > tweet.sh
 chmod +x tweet.sh
 while read entry; do
@@ -12,3 +14,4 @@ COMMITID=$(git log -1 2>/dev/null | grep -i "commit" | sed -e "s/commit //g")
 COMMITURL=$(echo "https://github.com/ixt/I-am-awake-I-am-alive-I-am-orange/commit/$COMMITID")
 ./tweet.sh "Verification: $COMMITURL"
 rm ./tweet.sh
+. torsocks off
