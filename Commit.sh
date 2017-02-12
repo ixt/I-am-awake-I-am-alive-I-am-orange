d6ff968f13ad06634cc5371a3461bff91bf05c10
NfN Orange <orange@ff4500.red>
2017-02-12 01:01:19 +0000

    stop that redundancy!

--- Commit.sh
+++ Commit.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
-# 2017-02-12 00:46:41
-. torsocks on
+#!/usr/bin/env bash
+# 2017-02-12 00:52:24
 git add README.md 2>/dev/null
 git commit -sm "$(date -u +%Y-%m-%d\ %H:%M:%S)" 2>/dev/null
-for r in $(git remote); do pkill gpg-agent; gpg-agent --enable-ssh-support --daemon git push $r master; done
+for r in $(git remote); do git push $r master; done
 gpg -d tweet.sh.gpg > tweet.sh
 chmod +x tweet.sh
 while read entry; do
@@ -13,4 +12,3 @@ COMMITID=$(git log -1 2>/dev/null | grep -i "commit" | sed -e "s/commit //g")
 COMMITURL=$(echo "https://github.com/ixt/I-am-awake-I-am-alive-I-am-orange/commit/$COMMITID")
 ./tweet.sh "Verification: $COMMITURL"
 rm ./tweet.sh
-. torsocks off 
