7330295022f101f6ff4350b30fee47ec62a6a692
NfN Orange <orange@ff4500.red>
2017-02-12 00:47:41 +0000

    cleaning of commit.sh and the redundant file
    Signed-off-by: NfN Orange <orange@ff4500.red>

--- Commit.sh
+++ Commit.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-# 2017-01-16 23:26:02
-REPOS=( origin ff4500 github )
+# 2017-02-12 00:46:41
 . torsocks on
 git add README.md 2>/dev/null
 git commit -sm "$(date -u +%Y-%m-%d\ %H:%M:%S)" 2>/dev/null
-for i in "${REPOS[@]}"; do
-    pkill gpg-agent
-    gpg-agent --enable-ssh-support --daemon git push $i master
-done
+for r in $(git remote); do pkill gpg-agent; gpg-agent --enable-ssh-support --daemon git push $r master; done
 gpg -d tweet.sh.gpg > tweet.sh
+chmod +x tweet.sh
 while read entry; do
    ./tweet.sh "$(echo $entry | sed -s "s/# //g")"
 done < README.md
 COMMITID=$(git log -1 2>/dev/null | grep -i "commit" | sed -e "s/commit //g")
 COMMITURL=$(echo "https://github.com/ixt/I-am-awake-I-am-alive-I-am-orange/commit/$COMMITID")
 ./tweet.sh "Verification: $COMMITURL"
-echo "" > tweet.sh
+rm ./tweet.sh
 . torsocks off 
