488c081721c45484b919c819375074d79f0ebc9c
NfN Orange <orange@ff4500.red>
2017-03-27 22:35:35 +0100

    2017-03-27 21:35:35
    Signed-off-by: NfN Orange <orange@ff4500.red>

--- Commit.sh
+++ Commit.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-# 2017-03-14 23:08:14
+# 2017-03-27 21:34:14
 git add README.md 2>/dev/null
 git commit -sm "$(date -u +%Y-%m-%d\ %H:%M:%S)" 2>/dev/null
 for r in $(git remote); do git push $r master; done
 gpg --card-status 2>/dev/null
 gpg -d tweet.sh.gpg > tweet.sh
 chmod +x tweet.sh
-. torsocks on
 while read entry; do
    ./tweet.sh "$(echo $entry | sed -s "s/# //g")"
 done < README.md
@@ -14,4 +13,3 @@ COMMITID=$(git log -1 2>/dev/null | grep -i "commit" | sed -e "s/commit //g")
 COMMITURL=$(echo "https://github.com/ixt/I-am-awake-I-am-alive-I-am-orange/commit/$COMMITID")
 ./tweet.sh "Verification: $COMMITURL"
 rm ./tweet.sh
-. torsocks off
