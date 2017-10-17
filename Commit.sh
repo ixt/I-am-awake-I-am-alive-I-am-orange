ecab1eada0fdc8e83ab0aefed87769361855d34a
NfN Orange <orange@ff4500.red>
2017-10-18 00:30:37 +0100

    Update and disclosure
    Signed-off-by: NfN Orange <orange@ff4500.red>

--- Commit.sh
+++ Commit.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-# 2017-08-01 08:54:49
+# 2017-10-17 23:24:45
 cd $(dirname $0)
 git add README.md 
 git commit -sm "$(date -u +%Y-%m-%d\ %H:%M:%S)"
 for r in $(git remote); do git push $r master; done
-gpg --card-status 
-gpg -d tweet.sh.gpg > tweet.sh
+gpg2 --card-status 
+gpg2 -d tweet.sh.gpg > tweet.sh
 chmod +x tweet.sh
 while read entry; do
    ./tweet.sh "$(echo $entry | sed -s "s/# //g")"
 done < README.md
 COMMITID=$(git log -1 2>/dev/null | grep -i "commit" | sed -e "s/commit //g")
 COMMITURL=$(echo "https://github.com/ixt/I-am-awake-I-am-alive-I-am-orange/commit/$COMMITID")
-./tweet.sh "Verification: $COMMITURL"
-rm ./tweet.sh
+#./tweet.sh "Verification: $COMMITURL"
+#rm ./tweet.sh
 cd -
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # I am awake 
 # I am alive
-# I am hello
+# I am currently fixing this project
+
+There may be no automatic tweets for a day or so relating to this,
+This was due to CVE 2017-15361 and my stupidity around generating keys on my yubikey. I will attempt to sign this repo everyday on the public facing repos I maintain:
+
+https://git.ff4500.red/I-am-awake-I-am-alive-I-am-orange.git
+https://github.com/ixt/I-am-awake-I-am-alive-I-am-orange.git 
